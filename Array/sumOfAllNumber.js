a17fa50aa6bf2a427b0a11ee7856a2cb2c94cbff
jayeshk-star <jayeshkale052@gmail.com>
2023-12-22 09:54:30 +0530

    add sum of all number programe 1

---
+++ Array/sumOfAllNumber.js
@@ -0,0 +1,15 @@
+// sum of all number
+const array = [1, 2, 3, 4, 56, 8, 9, 4, 5, 54];
+
+// method 1
+let sum = 0;
+for (let i = 0; i < array.length; i++) {
+  sum += array[i];
+}
+// method 2 reduce function
+
+const newsum = array.reduce((add, pre) => {
+  return add + pre;
+}, 0);
+
+console.log("sum", newsum);
